8d7456b514132fad2c59ea54e0a6e169b8c313eb
Hugo <carnielhugo8@gmail.com>
2020-11-01 16:37:08 -0300

    Do the add new todo

--- app.js
+++ app.js
@@ -7,4 +7,23 @@ app.use(morgan("dev"));
 
 app.use(express.static("public"));
 
+var indice_atual = 0;
+
+function Todo(name, completed) {
+  var id = indice_atual;
+  indice_atual++;
+  return { id, name, completed };
+}
+
+var todos = [Todo("Comprar Peixe", false), Todo("Comprar Carne", true)];
+
+// GET /api/todos => lista de todas as Todos
+app.get("/api/todos", (req, res) => {
+  res.json(todos);
+});
+// POST /api/todos => cria uma nova todo
+// GET /api/todos/5 => exibe detalhes da Todo
+// PUT /api/todos/5 => edita a todo
+// DELETE /api/todos/5 => Apaga a Todo 5
+
 app.listen(port);
