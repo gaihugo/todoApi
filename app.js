31afd6876b40acec0906d88d74f0a21523cb8416
Hugo <carnielhugo8@gmail.com>
2021-01-31 17:18:29 -0300

    refatorado

--- app.js
+++ app.js
@@ -5,12 +5,15 @@ const app = express();
 const port = 3000;
 const mongoose = require("mongoose");
 require("dotenv/config");
+const { todoRouter, Todo } = require("./todo");
 
 app.use(morgan("dev"));
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+// Mongoose
 
 mongoose
   .connect(process.env.CONECTION_STRING, {
@@ -20,11 +23,6 @@ mongoose
   .then(() => console.log("OK!!"))
   .catch((e) => console.error("ERROR", e));
 
-const Todo = mongoose.model("Todo", {
-  name: String,
-  completed: Boolean,
-});
-
 const User = mongoose.model("User", {
   username: String,
   passwordHash: String,
@@ -32,50 +30,7 @@ const User = mongoose.model("User", {
   email: String,
 });
 
-// GET /api/todos => lista de todas as Todos
-app.get("/api/todos/", (req, res) => {
-  Todo.find({}).then((todos) => res.json(todos));
-});
-
-// POST /api/todos => cria uma nova todo
-app.post("/api/todos/", (req, res) => {
-  const todo = new Todo({
-    name: req.body.text,
-    completed: false,
-  });
-  todo.save().then(() => {
-    res.json({ status: "Success" });
-  });
-});
-
-// GET /api/todos/5 => exibe detalhes da Todo
-app.get("/api/todos/:id/", (req, res) => {
-  // req.params.id
-  Todo.findById(req.params.id).then((todo) => {
-    res.json(todo);
-  });
-});
-
-// PUT /api/todos/5 => edita a todo
-app.put("/api/todos/:id/", (req, res) => {
-  // req.params.id
-  Todo.findById(req.params.id).then((todo) => {
-    todo.name = req.body.text || todo.name;
-    todo.completed = req.body.completed;
-    todo
-      .save()
-      .then(() => {
-        res.json({ status: "Success" });
-      })
-      .catch((e) => console.error(e));
-  });
-});
-
-// DELETE /api/todos/5 => Apaga a Todo 5
-app.delete("/api/todos/:id/", async (req, res) => {
-  await Todo.deleteOne({ _id: req.params.id });
-
-  res.json({ status: "Success" });
-});
+// Rotas
+app.use("/api/todos", todoRouter);
 
 app.listen(port);
---
+++ todo.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const router = express.Router();
+const mongoose = require("mongoose");
+require("dotenv/config");
+
+const Todo = mongoose.model("Todo", {
+  name: String,
+  completed: Boolean,
+});
+
+// GET /api/todos => lista de todas as Todos
+router.get("/", (req, res) => {
+  Todo.find({}).then((todos) => res.json(todos));
+});
+
+// POST /api/todos => cria uma nova todo
+router.post("/", (req, res) => {
+  const todo = new Todo({
+    name: req.body.text,
+    completed: false,
+  });
+  todo.save().then(() => {
+    res.json({ status: "Success" });
+  });
+});
+
+// GET /api/todos/5 => exibe detalhes da Todo
+router.get("/:id/", (req, res) => {
+  // req.params.id
+  Todo.findById(req.params.id).then((todo) => {
+    res.json(todo);
+  });
+});
+
+// PUT /api/todos/5 => edita a todo
+router.put("/:id/", (req, res) => {
+  // req.params.id
+  Todo.findById(req.params.id).then((todo) => {
+    todo.name = req.body.text || todo.name;
+    todo.completed = req.body.completed;
+    todo
+      .save()
+      .then(() => {
+        res.json({ status: "Success" });
+      })
+      .catch((e) => console.error(e));
+  });
+});
+
+// DELETE /api/todos/5 => Apaga a Todo 5
+router.delete("/:id/", async (req, res) => {
+  await Todo.deleteOne({ _id: req.params.id });
+
+  res.json({ status: "Success" });
+});
+
+module.exports = {
+  todoRouter: router,
+  Todo,
+};
